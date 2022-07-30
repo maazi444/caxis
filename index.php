1dd89cbb297f4f3205fce1a1cc7672fd26505200
M Maaz Ahmad <95238151+maazi444@users.noreply.github.com>
2022-07-29 17:32:53 -0700

    caxis website v1.0

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/12583d8ffd.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Caxis</title>
+</head>
+<body>
+    <main>
+        <header>
+            <div class="logoBrand">
+                <a href="index.php">
+                    <img src="assets/logoText.png" alt="Caxis"/>
+                </a>
+            </div>
+            <ul class="menuLinks">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </header>
+        <section class="main-section">
+            <img src="assets/boost-business.png" alt="Boost Your business with Caxis"/>
+            <article class="main-article">
+                <h1 class="main-heading">The easiest way<br/>to start your business<br/>in USA.</h1>
+                <p class="main-paragraph">We are the incorporation experts -- it's fast, easy, affordable and done right.</p>
+                <div class="newsletter-box">
+                    <p class="newsletter-paragraph">Ready to form an LLC, Corporation, or Non-Profit?</p>
+                    <a href="#" class="newsletter-btn">Get Started</a>
+                </div>
+            </article>
+        </section>
+        <section class="join-us-section">
+            <article class="join-us-article">
+                <h1 class="join-us-heading">Join Us<br/>in this exciting journey</h1>
+                <p class="join-us-paragraph">
+                    We know that our success grows from your success, so we strive to fulfill your business needs with enthusiasm, speed, and absolute professionalism. We won't treat you like Joe Schmoe. We'll listen to you like a business partner.
+                </p>
+            </article>
+            <img src="assets/join-us-image.png" alt="Join Caxis">
+        </section>
+        <section class="know-more-section">
+            <img src="assets/know-more-image.png" class="know-more-image" alt="Know more about Caxis">
+            <article class="know-more-article">
+                <p class="know-more-paragraph">Lorem ipsum dolor sit amet</p>
+                <h1 class="know-more-heading">Know<br/>more about us</h1>
+                <div class="know-more-box">
+                    <div class="know-more-row">
+                        <i class='far fa-check-circle'></i>
+                        <p>In scelerisque sem at dolor</p>
+                    </div>
+                    <div class="know-more-row">
+                        <i class='far fa-check-circle'></i>
+                        <p>Lorem ipsum dolor sit amet</p>
+                    </div>
+                    <div class="know-more-row">
+                        <i class='far fa-check-circle'></i>
+                        <p>Class aptent taciti sociosqu</p>
+                    </div>
+                    <div class="know-more-row">
+                        <a href="#" class="about-paragraph">Want to know more about us?</a>
+                    </div>
+                </div>
+            </article>
+        </section>
+        <section class="contact-footer-outer">
+            <article class="contact-footer">
+                <div class="contact-section">
+                    <img src="assets/contact-us-image.png" class="contact-us-image" alt="Contact caxis">
+                    <article class="contact-us-article">
+                        <h1 class="contact-us-heading">Contact<br/>with us.</h1>
+                        <div class="contact-us-row">
+                            <i class='fas fa-envelope' style='font-size:24px'></i>
+                            <p>loremipsum@gmail.com</p>
+                        </div>
+                        <div class="contact-us-row">
+                            <i class='fas fa-map-marker-alt' style='font-size:24px'></i>
+                            <p>Lane 4, Winston Road, NY</p>
+                        </div>
+                        <div class="contact-us-row">
+                            <i class='fas fa-phone-alt' style='font-size:24px'></i>
+                            <p>+1 3423 4232234</p>
+                        </div>
+                    </article>
+                </div>
+                <div class="footer-section">
+                    <div class="logoBrand">
+                        <a href="index.php">
+                            <img src="assets/text.png" alt="Caxis"/>
+                        </a>
+                    </div>
+                    <ul class="footerLinks">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+
+                    <ul class="social-links">
+                        <li><a href="#"><i class='fab fa-facebook' style='font-size:24px'></i></a></li>
+                        <li><a href="#"><i class='fab fa-instagram' style='font-size:24px'></i></a></li>
+                        <li><a href="#"><i class='fab fa-youtube' style='font-size:24px'></i></a></li>
+                    </ul>
+                </div>
+                <p class="copyright-text">Copyright &copy; 2022, Microcomsols</p>
+            </article>
+        </section>
+    </main>
+</body>
+</html>
